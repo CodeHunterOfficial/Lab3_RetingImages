030eaeadcada64c296a45f6906e281fa02c804cd
Arabov Mullosharaf <kurbonpur@bk.ru>
2021-10-30 13:34:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/graphapplication/Photo.java
@@ -0,0 +1,10 @@
+package com.example.graphapplication;
+
+public class Photo {
+    public  String name;
+    public Boolean rating;
+    public Photo(String name) {
+        this.name=name;
+        rating =false;
+    }
+}
